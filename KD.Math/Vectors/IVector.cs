80376eb5b741585b5c0641e8396191162bd0b50f
Krzysztof Dobrzyński <k.dobrzynski94@gmail.com>
2018-03-30 07:08:54 +0200

    Renamed generic value to 'T'.

--- KD.Math/Vectors/IVector.cs
+++ KD.Math/Vectors/IVector.cs
@@ -6,86 +6,86 @@ namespace KD.Math.Vectors
     /// <summary>
     /// General vector definition.
     /// </summary>
-    /// <typeparam name="TDataType">  </typeparam>
-    public interface IVector<TDataType> : 
-        IEnumerable<TDataType>, IComparable<IVector<TDataType>>, IEquatable<IVector<TDataType>>
-        where TDataType : IComparable<TDataType>
+    /// <typeparam name="T">  </typeparam>
+    public interface IVector<T> : 
+        IEnumerable<T>, IComparable<IVector<T>>, IEquatable<IVector<T>>
+        where T : IComparable<T>
     {
         /// <summary>
         /// Returns value at specified index in current vector.
         /// </summary>
         /// <param name="index"></param>
         /// <returns></returns>
-        IVector<TDataType> this[int index] { get; }
+        T this[int index] { get; set; }
 
         /// <summary>
         /// Returns the number of elements in current vector.
         /// </summary>
-        TDataType Count { get; }
+        int Count { get; }
         /// <summary>
         /// Returns the length of current vector.
         /// </summary>
-        TDataType Length { get; }
+        T Length { get; }
 
         /// <summary>
         /// Returns current vector but with all the values being absoluted.
         /// </summary>
         /// <returns></returns>
-        IVector<TDataType> Absolute();
+        IVector<T> Absolute();
         /// <summary>
         /// Returns new vector which is a sum of current vector and vector specified in argument.
         /// </summary>
         /// <param name="vector"> Vector which will be added to current vector and create a sum of both. </param>
         /// <returns></returns>
-        IVector<TDataType> Add(IVector<TDataType> vector);
+        IVector<T> Add(IVector<T> vector);
         /// <summary>
         /// Returns angle between current vector and vector specified in argument.
         /// </summary>
         /// <param name="vector"></param>
         /// <returns></returns>
-        double Angle(IVector<TDataType> vector);
+        double Angle(IVector<T> vector);
         /// <summary>
         /// Returns cross product of current vector and vector specified in argument.
         /// </summary>
         /// <param name="vector"></param>
         /// <returns></returns>
-        IVector<TDataType> CrossProduct(IVector<TDataType> vector);
+        IVector<T> CrossProduct(IVector<T> vector);
         /// <summary>
         /// Returns dot product of current vector and vector specified in argument.
         /// </summary>
         /// <param name="vector"></param>
         /// <returns></returns>
-        IVector<TDataType> DotProduct(IVector<TDataType> vector);
+        IVector<T> DotProduct(IVector<T> vector);
         /// <summary>
         /// Initializes current vector with value from specified enumerable.
         /// Each call of this method will remove all current data inside current vector and reinitialize it from specified.
         /// </summary>
         /// <param name="data"> Data from which vector should be created </param>
         /// <returns></returns>
-        IVector<TDataType> Initialize(IEnumerable<TDataType> data);
+        void Initialize(IEnumerable<T> data);
         /// <summary>
         /// Returns vector which is a product of multiplying current vector with specified in argument.
         /// </summary>
         /// <param name="vector"></param>
         /// <returns></returns>
-        IVector<TDataType> Multiply(IVector<TDataType> vector);
+        IVector<T> Multiply(IVector<T> vector);
         /// <summary>
         /// Returns normalized current vector.
         /// </summary>
         /// <returns></returns>
-        IVector<TDataType> Normalize();
+        IVector<T> Normalize();
         /// <summary>
         /// Returns vector which is a product of subtracting specified vector from current.
         /// </summary>
         /// <param name="vector"> Vector which will be subtracted from current. </param>
         /// <returns></returns>
-        IVector<TDataType> Subtract(IVector<TDataType> vector);
+        IVector<T> Subtract(IVector<T> vector);
         /// <summary>
         /// Converts current vector to unit vector.
         /// 
         /// NOTE: Unit vector is a vector which length is equal '1' (one).
         /// </summary>
         /// <returns></returns>
-        IVector<TDataType> ToUnit();
+        IVector<T> ToUnit();
     }
 }
